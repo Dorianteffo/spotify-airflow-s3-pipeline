6b0b361b41900a11294dc4788d99f1fe809b3fd9
Dorian <dorianteffo@gmail.com>
2023-08-16 09:18:59 +0100

    modify the ETL file

--- spotify_etl.py
+++ spotify_etl.py
@@ -9,19 +9,11 @@ def etl_track():
     df = pd.DataFrame(df)
 
     #data cleaning 
-
     #convert explicit column to bool
     df['explicit'] = df['explicit'].replace({"0.0": False,"1.0": True, 0.0 : "False", 1.0 : "True"})
     df['explicit'] = df['explicit'].astype('bool')
 
-    #strip unnecessary characters
-    char_strip = ["[", "]", "'", '"']
-    for char in char_strip : 
-        df['artists_name'] = df['artists_name'].str.replace(char, '')
-        df['available_markets'] = df['available_markets'].str.replace(char, '')
-        df['artists_id'] = df['artists_id'].str.replace(char, '')
-
-    df.to_csv('s3://dorian-spotify-api/tracks_data')
+    df.to_csv('s3://dorian-spotify-api/tracks_data.csv')
 
 
 def etl_album(): 
@@ -31,15 +23,8 @@ def etl_album():
     df = pd.DataFrame(df)
 
     # data cleaning 
-    
     #convert the release_date to datetime 
     df['release_date'] = pd.to_datetime(df['release_date'])
-    
-    #strip unnecessary characters
-    char_strip = ["[", "]", "'", '"']
-    for char in char_strip : 
-        df['artist_name'] = df['artist_name'].str.replace(char, '')
-        df['available_markets'] = df['available_markets'].str.replace(char, '')
-        df['artist_id'] = df['artist_id'].str.replace(char, '')
+
     df.to_csv('s3://dorian-spotify-api/album_data.csv')
 
